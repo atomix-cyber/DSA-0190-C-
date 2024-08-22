c64e8a3e71426f315ddc7c546a4f1041cff3e3b1
MOKESH KUMAR <121533632+atomix-cyber@users.noreply.github.com>
2024-08-22 09:08:41 +0530

    Add files via upload

---
+++ Check whether the interger is prime or not.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int number;
+    bool isPrime = true;
+    cout << "Enter an integer: ";
+    cin >> number;
+    if (number <= 1) {
+        isPrime = false;
+    } else {
+        for (int i = 2; i * i <= number; i++) {
+            if (number % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+    }
+    if (isPrime) {
+        cout << number << " is a prime number." << endl;
+    } else {
+        cout << number << " is not a prime number." << endl;
+    }
+    return 0;
+}
